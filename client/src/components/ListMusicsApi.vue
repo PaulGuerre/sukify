b762b4af5d950a59d15b0cf9378f438ab8209885
Paulobergine <pguerreftb@gmail.com>
2022-03-22 23:46:32 -0400

    Add play/pause button management

--- client/src/components/ListMusicsApi.vue
+++ client/src/components/ListMusicsApi.vue
@@ -1,16 +1,17 @@
 <template>
   <div id="musicList">
-    <div class="row row-cols-md-4 row-cols-lg-6">
+    <div class="row row-cols-md-4 row-cols-lg-5">
       <div class="col" v-for="music in musics" :key="music.id">
         <div class="card text-center">
           <img :src="'http://localhost:3000/musics/' + music.id + '/thumbnail'" class="card-img-top">
           <div class="card-body">
-            <p v-if="music.title.length < 50" class="card-text">{{ music.title }}</p>
-            <p v-else class="card-text">{{ music.title.substring(0, 50) + "..." }}</p>
+            <p v-if="music.title.length < 15" class="card-text">{{ music.title }}</p>
+            <p v-else class="card-text">{{ music.title.substring(0, 15) + "..." }}</p>
           </div>
           <div class="card-footer">
             <div class="btn-group" role="group">
-              <PlayMusic :id="music.id" />
+              <button v-if="this.playStatus === music.id" class="btn btn-success" id="playButton" type="button" @click="pause(music.id)"><i  class="fas fa-pause"></i></button>
+              <button v-else class="btn btn-success" id="playButton" type="button" @click="play(music.id)"><i  class="fas fa-play"></i></button>              <!--<PlayMusic :id="music.id" :audio.sync="audio" />-->
               <button class="btn btn-warning">Edit</button>
               <RemoveMusic :id="music.id" />
             </div>
@@ -23,18 +24,18 @@
 
 <script>
 import axios from 'axios'
-import PlayMusic from '../components/PlayMusicApi.vue'
 import RemoveMusic from '../components/RemoveMusicApi.vue'
 
 export default {
   name: 'ListMusic',
   components: {
-    PlayMusic,
     RemoveMusic
   },
   data () {
     return {
+      audio: new Audio(),
       connect: false,
+      playStatus: null,
       musics: [],
       errors: []
     }
@@ -49,6 +50,18 @@ export default {
         .catch(e => {
           this.error.push(e)
         })
+    },
+    play (id) {
+      if (this.audio.src !== 'http://localhost:3000/musics/' + id) {
+        this.audio.src = 'http://localhost:3000/musics/' + id
+        this.audio.load()
+      }
+      this.playStatus = this.audio.paused ? id : null
+      this.audio.play()
+    },
+    pause (id) {
+      this.playStatus = this.audio.paused ? id : null
+      this.audio.pause()
     }
   },
   created () {
