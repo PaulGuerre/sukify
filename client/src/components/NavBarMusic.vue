ec533c9ac85860a6838365048fe4b79aef5d153f
Paulobergine <pguerreftb@gmail.com>
2022-04-06 16:03:40 -0400

    navbar update

--- client/src/components/NavBarMusic.vue
+++ client/src/components/NavBarMusic.vue
@@ -1,23 +1,27 @@
 <template>
-  <nav class="navbar fixed-top bg-success" id="sex">
+  <nav class="navbar navbar-expand-sm fixed-top bg-success" id="sex">
     <div class="container-fluid">
       <router-link to="/" class="navbar-brand text-light">Home</router-link>
-      <form v-if="connect" class="d-flex">
-        <div class="btn-group btn-group-sm" role="group">
-          <PreviousMusicButton v-on:previous="previousMusic" />&nbsp;
-          <PlayMusic class="bg-light text-success" :id="currentMusic" :audio="audio" v-on:play="play($event)" v-on:pause="pause()" :playStatus="playStatus" />&nbsp;
-          <NextMusicButton v-on:next="nextMusic" />&nbsp;
-          <RepeatMusic :playMode="playMode" v-on:repeat="repeatMode($event)" />&nbsp;
-          <RandomMusic :playMode="playMode" v-on:random="randomMode($event)" />
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="text-light"><i class="fas fa-sliders-h"></i></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <router-link to="/playlists" class="navbar-nav me-auto nav-link text-light">Playlists</router-link>
+          <div v-if="connect" class="d-flex btn-group btn-group-sm" role="group">
+            <PreviousMusicButton v-on:previous="previousMusic" />&nbsp;
+            <PlayMusic class="bg-light text-success" :id="currentMusic" :audio="audio" v-on:play="play($event)" v-on:pause="pause()" :playStatus="playStatus" />&nbsp;
+            <NextMusicButton v-on:next="nextMusic" />&nbsp;
+            <RepeatMusic :playMode="playMode" v-on:repeat="repeatMode($event)" />&nbsp;
+            <RandomMusic :playMode="playMode" v-on:random="randomMode($event)" />
+          </div>
+          <div v-else class="d-flex btn-group btn-group-sm placeholder-wave">
+            <button class="btn btn-light disabled placeholder"><i class="fas fa-angle-double-left"></i></button>&nbsp;
+            <button class="btn btn-light disabled placeholder"><i class="fas fa-play"></i></button>&nbsp;
+            <button class="btn btn-light disabled placeholder"><i class="fas fa-angle-double-right"></i></button>&nbsp;
+            <button class="btn btn-light disabled placeholder"><i class="fas fa-redo"></i></button>&nbsp;
+            <button class="btn btn-light disabled placeholder"><i class="fas fa-random"></i></button>&nbsp;
+          </div>
         </div>
-      </form>
-      <form v-else class="d-flex placeholder-wave">
-        <button class="btn btn-light disabled placeholder"><i class="fas fa-angle-double-left"></i></button>&nbsp;
-        <button class="btn btn-light disabled placeholder"><i class="fas fa-play"></i></button>&nbsp;
-        <button class="btn btn-light disabled placeholder"><i class="fas fa-angle-double-right"></i></button>&nbsp;
-        <button class="btn btn-light disabled placeholder"><i class="fas fa-redo"></i></button>&nbsp;
-        <button class="btn btn-light disabled placeholder"><i class="fas fa-random"></i></button>&nbsp;
-      </form>
     </div>
   </nav>
 </template>
