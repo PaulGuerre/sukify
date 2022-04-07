2d881420fccce357194a090a51dcc8f7586e4085
Paulobergine <pguerreftb@gmail.com>
2022-04-06 21:44:13 -0400

    Test function for headset controls

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -110,6 +110,12 @@ export default {
           break
       }
     })
+    this.audio.addEventListener('pause', () => {
+      this.pause()
+    })
+    this.addEventListener('play', () => {
+      this.play(this.currentMusic)
+    })
   }
 }
 </script>
