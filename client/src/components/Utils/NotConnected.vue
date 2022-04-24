00aeec91087adb867d0e28587be89d887aa37af8
Paulobergine <pguerreftb@gmail.com>
2022-04-24 17:39:37 -0400

    NotConnected componet style update

--- client/src/components/Utils/NotConnected.vue
+++ client/src/components/Utils/NotConnected.vue
@@ -1,24 +1,24 @@
 <template>
   <div>
-    <nav class="navbar navbar-expand-sm fixed-top bg-success">
+    <nav class="navbar navbar-expand-sm fixed-top" style="background-color: #212121">
       <div class="container-fluid">
-        <router-link to="/" class="navbar-brand text-light">Sukify</router-link>
+        <router-link to="/" class="navbar-brand text-success">Sukify</router-link>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="text-light"><i class="fas fa-sliders-h"></i></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <div class="container-fluid">
             <div class="row">
-              <button class="btn bg-success btn-link text-light border-0 col-6 col-md-2 col-lg-1 disabled">Playlists</button>
-              <button class="btn bg-success btn-link text-light border-0 col-6 col-md-2 col-lg-1 disabled">Musics</button>
+              <button class="btn btn-link text-success border-0 col-6 col-md-2 col-lg-1 disabled" style="background-color: #212121">Playlists</button>
+              <button class="btn btn-link text-success border-0 col-6 col-md-2 col-lg-1 disabled" style="background-color: #212121">Musics</button>
             </div>
           </div>
-          <div class="d-flex btn-group btn-group-sm placeholder-wave" role="group">
-            <button class="btn btn-light text-success disabled placeholder" type="button"><i class="fas fa-angle-double-left"></i></button>&nbsp;
-            <button class="btn btn-light text-success disabled placeholder" type="button"><i class="fas fa-play"></i></button>&nbsp;
-            <button class="btn btn-light text-success disabled placeholder" type="button"><i class="fas fa-angle-double-right"></i></button>&nbsp;
-            <button class="btn btn-light text-success disabled placeholder" type="button"><i class="fas fa-redo"></i></button>&nbsp;
-            <button class="btn btn-light text-success disabled placeholder" type="button"><i class="fas fa-random"></i></button>
+          <div class="d-flex btn-group btn-group-sm border border-success placeholder-wave" role="group">
+            <button class="btn btn-light text-success disabled placeholder" style="background-color: #212121" type="button"><i class="fas fa-angle-double-left"></i></button>&nbsp;
+            <button class="btn btn-light text-success disabled placeholder" style="background-color: #212121" type="button"><i class="fas fa-play"></i></button>&nbsp;
+            <button class="btn btn-light text-success disabled placeholder" style="background-color: #212121" type="button"><i class="fas fa-angle-double-right"></i></button>&nbsp;
+            <button class="btn btn-light text-success disabled placeholder" style="background-color: #212121" type="button"><i class="fas fa-redo"></i></button>&nbsp;
+            <button class="btn btn-light text-success disabled placeholder" style="background-color: #212121" type="button"><i class="fas fa-random"></i></button>
           </div>
         </div>
       </div>
@@ -28,7 +28,7 @@
 
     <div class="row row-cols-1 row-cols-md-3 row-cols-lg-6" style="margin-top: 10vh; margin-right: 2%; margin-left: 2%;">
       <div class="col" v-for="index in 5" :key="index">
-        <div class="card bg-dark bg-inverse text-success border border-success mb-4">
+        <div class="card text-success mb-4 shadow-lg" style="background-color: #424242">
           <img src="https://i.ytimg.com/vi/xN6LFM4CkWI/mqdefault.jpg" class="card-img-top placeholder placeholder-wave">
           <div class="card-img-overlay placeholder-wave">
             <h5 class="card-title fs-3 text-truncate placeholder col-7"></h5><br />
