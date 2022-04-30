4a9b03bee253b9dd7db6ef7d332e4d7fae96e985
Paulobergine <pguerreftb@gmail.com>
2022-04-30 14:27:55 -0400

    Update cors for security

--- server/config/initializers/cors.rb
+++ server/config/initializers/cors.rb
@@ -1,8 +1,17 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins '*'
-        resource '*',
+        origins 'http://localhost:8080'
+        resource '/musics',
             headers: :any,
             methods: [:get, :post, :delete, :put]
+        resource '/playlists',
+            headers: :any,
+            methods: [:get, :post, :delete, :put]
+        resource '/token',
+            headers: :any,
+            methods: [:post]
+        resource '/login',
+            headers: :any,
+            methods: [:post]
     end
 end
