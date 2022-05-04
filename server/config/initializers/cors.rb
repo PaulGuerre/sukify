2a2d85026c568bd615b72d1d987d88b92c6183a5
Paul Guerre <pguerreftb@gmail.com>
2022-05-04 05:17:29 +0200

    docker-compose update + cors bug removal

--- server/config/initializers/cors.rb
+++ server/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins 'http://localhost:8080'
+        origins '*'
         resource '*',
             headers: :any,
             methods: [:get, :post, :delete, :put]
