371bc75b9bc473d056fb20831e15b08ed232326d
Paul Guerre <pguerreftb@gmail.com>
2022-08-11 16:13:31 +0200

    Ui update : spinner modal shadow removed

--- client/src/components/Add.vue
+++ client/src/components/Add.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="add">
     <div class="modal fade" id="addMusicModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-hidden="true">
-      <div class="modal-dialog modal-dialog-centered modal-sm border border-0">
-        <div class="modal-content" style="background-color: transparent;">
+      <div class="modal-dialog modal-dialog-centered modal-sm">
+        <div class="modal-content shadow-none" style="background-color: transparent;">
           <button type="button" class="btn-close" id="closeModal" data-bs-dismiss="modal" aria-label="Close" hidden></button>
           <div class="text-center">
             <div class="spinner-border text-success" style="width: 5rem; height: 5rem;" role="status">
